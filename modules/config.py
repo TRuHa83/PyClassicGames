4caee4bf1952d93f082e7b5bbe807c0cf185a1d3
Sergio Trujillo <sergiotrujillonuez@gmail.com>
2026-01-24 09:39:22 +0100

    Se agrega el módulo `config.py` para gestión de rutas y carga de configuraciones.

---
+++ modules/config.py
@@ -0,0 +1,39 @@
+import os
+import json
+
+from pathlib import Path
+
+
+class Config:
+    def __init__(self, ui_main):
+        self.ui_main = ui_main
+        self.work_path = Path(os.getcwd())
+        self.config_file = self.work_path / "config.json"
+
+        self.setting = self.get_setting()
+        self._update_setting()
+
+    def _update_setting(self):
+        update = self.setting.get("update")
+        self.ui_main.actionCheckRun.setChecked(update)
+
+    def _set_default_setting(self):
+        self.default_setting = {
+            "update": True
+        }
+
+        self.set_setting(self.default_setting)
+
+    def get_path(self):
+        return self.work_path
+
+    def get_setting(self):
+        if not self.config_file.exists():
+            self._set_default_setting()
+
+        with open(self.config_file, "r") as f:
+            return json.load(f)
+
+    def set_setting(self, setting):
+        with open(self.config_file, "w") as f:
+            json.dump(setting, f)
