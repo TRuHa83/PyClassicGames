0722606db607c54f840f28d1456b4f8b89f23c35
Sergio Trujillo <sergiotrujillonuez@gmail.com>
2026-01-26 16:12:13 +0100

    Actualización a la versión 0.9.8

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 __github_url__ = "https://github.com/TRuHa83/PyClassicGames"
 __author__     = "Sergio Trujillo de la Nuez"
 __app_name__   = "PyClassicGames"
-__version__    = "0.9.7"
+__version__    = "0.9.8"
 __year__       = "2026"
